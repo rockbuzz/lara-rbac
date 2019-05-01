ffda917ac956620ca7e19d2baad10d14ce1dff2c
tiagorockbuzz <tiago@rockbuzz.com.br>
2019-05-01 11:51:46 -0300

    diretivas para blade

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Rockbuzz\LaraRbac;
 
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider as SupportServiceProvider;
 
 class ServiceProvider extends SupportServiceProvider
@@ -17,10 +18,39 @@ class ServiceProvider extends SupportServiceProvider
         $this->publishes([
             __DIR__ . '/config/rbac.php' => config_path('rbac.php')
         ], 'config');
+
+        Blade::if('hasrole', function($expression){
+            if (!strpos($expression, ',')) {
+                return auth()->user()->hasRole($expression);
+            }
+            list($roles, $company) = $this->parseExpression($expression);
+            return auth()->user()->hasRole($roles, $company);
+        });
+        Blade::if('haspermission', function($expression){
+            if (!strpos($expression, ',')) {
+                return auth()->user()->hasPermission($expression);
+            }
+            list($permissions, $company) = $this->parseExpression($expression);
+            return auth()->user()->hasPermission($permissions, $company);
+        });
     }
 
     public function register()
     {
         $this->mergeConfigFrom(__DIR__ . '/config/rbac.php', 'rbac');
+    }
+
+    private function parseExpression(string $expression): array
+    {
+        return $this->takeOutSpaces(
+            explode(',', $expression)
+        );
+    }
+
+    private function takeOutSpaces(array $parameters): array
+    {
+        return array_map(function ($parameter) {
+            return trim($parameter);
+        }, $parameters);
     }
 }
